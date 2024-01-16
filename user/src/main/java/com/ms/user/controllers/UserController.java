0e665000fb0da22a03fc2cf1f10803924efebbb3
Pablo-Vini <pablovgramos@gmail.com>
2024-01-16 20:05:43 -0300

    Atualizando

--- user/src/main/java/com/ms/user/controllers/UserController.java
+++ user/src/main/java/com/ms/user/controllers/UserController.java
@@ -1,11 +1,13 @@
 package com.ms.user.controllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ms.user.dtos.UserRecordDto;
 import com.ms.user.models.UserModel;
 
 import jakarta.validation.Valid;
@@ -15,6 +17,8 @@ public class UserController {
 	
 	@PostMapping("/users")
 	public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto UserRecordDto){
+		var userModel = new UserModel();
+		BeanUtils.copyProperties(UserRecordDto, userModel);
 		return ResponseEntity.status(HttpStatus.CREATED).body();
 	}
 }
